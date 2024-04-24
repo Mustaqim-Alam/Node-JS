ec5004d6dff6d8008dc4aff10470055252dbb77d
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-04-24 10:23:42 +0530

    Module in NodeJS

---
+++ HelloWorld/Math.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+
+// console.log(add(3,5));
+
+// module.exports = add;
+// module.exports = sub;
+
+module.exports = {
+  add,
+  sub,
+};
--- HelloWorld/hello.js
+++ HelloWorld/hello.js
@@ -1,1 +1,7 @@
+const math = require('./Math')
+
 console.log("Starting nodejs learning ")
+
+console.log(math.sub(5,8));
+console.log(math.add(2,7));
+
