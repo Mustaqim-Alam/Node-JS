5315a7fbb54dd6c7dc320ded3499512ead4ceddc
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-04-30 11:12:54 +0530

    complete url

--- ServerJs/index.js
+++ ServerJs/index.js
@@ -1,20 +1,28 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()} ${req.url} New request recived \n `;
-  fs.appendFile("log.txt", log, (err, data)=>{
-    switch(req.url){
-      case '/' : res.end("Welcome to Home Page from server");
-      break;
-      case '/about' : res.end("Hello from About page This side Mustaqim");
-      break;
-      default : res.end("  404 Not Found!!")
-      
+  const myUrl = url.parse(req.url , true);
+  console.log(myUrl);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Welcome to Home Page from server");
+        break;
+      case "/about":
+        const username = myUrl.query.userName;
+        res.end(`Hello, ${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_name;
+        res.end(`You are searching for ${search}`);
+        break;
+      default:
+        res.end("  404 Not Found!!");
     }
   });
-
-  
 });
 
 myServer.listen(4500, () => console.log("server started"));
